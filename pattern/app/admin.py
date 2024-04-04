ad8361baf08aad1717859d7a70b8f757115674bd
VladimirVladimirovich83 <54549407+VladimirVladimirovich83@users.noreply.github.com>
2024-04-04 18:06:13 +0300

    t1
    t1

--- pattern/app/admin.py
+++ pattern/app/admin.py
@@ -8,5 +8,15 @@ class UserAdmin(admin.ModelAdmin):
     list_dispaly = ('phone', 'code', 'status', 'friend_code', 'status_code')
 
 
+class Candidats(models.Model):
+    first_name = models.CharField()
+    second_name = models.CharField()
+    adress = models.CharField(blank=True, null=True)
+    vacancy_id = models.IntegerField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'candidats'
+
 # Register your models here.
 admin.site.register(User, UserAdmin)
